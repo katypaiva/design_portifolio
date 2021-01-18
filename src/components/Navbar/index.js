d8e58a499fb342642292d805b2466606a76fae3d
Katy Paiva <katyfeitozaa@gmail.com>
2021-01-18 13:35:02 -0300

    inclusão do menu lateral

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,14 +5,31 @@ export default function Navbar() {
     return (
         <div class="container">
             <p class="p-name">Flávio Pires</p>
-            <div class="links-container">
-                <a>Sobre</a>
-                <a>Artigos</a>
-                <a>Projetos</a>
-                <a>Contato</a>
-            </div>
+            <input type="checkbox" id="check" />
+            <label for="check" >
+                <div class="menu-button">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect className="change-svg" opacity="0.5" width="24" height="24" rx="12" fill="#28282A"/>
+                        <rect className="change-svg" opacity="0.5" x="1.70068" y="13.7953" width="10.2992" height="10.2047" fill="#28282A"/>
+                        <rect className="change-svg" opacity="0.5" x="12" y="13.7953" width="10.2992" height="10.2047" fill="#28282A"/>
+                        <rect className="change-svg" opacity="0.5" x="12" y="3.59052" width="10.2992" height="10.2047" fill="#28282A"/>
+                        <rect className="change-svg" opacity="0.5" x="1.70068" y="3.59052" width="10.2992" height="10.2047" fill="#28282A"/>
+                    </svg>
+                </div>
+            </label>
             <p class="p-location">location </p>
-            <button class="menu-button"><img class="mg-btn" src={require('../../img/menu.svg')} /></button>
+            <ul>
+                <p className="name-small">Flávio Pires</p>
+                <p className="name-big">Flávio Pires</p>
+                <li><a className="about-href">Sobre</a></li>
+                <li><a className="article-href">Artigos</a></li>
+                <li><a className="project-href">Projetos</a></li>
+                <li><a className="contact-href">Contato</a></li>
+            </ul>
         </div>
+
     )
 }
+
+{/* <input type="checkbox" className="check" style={{display: none;}}/>
+ */}
