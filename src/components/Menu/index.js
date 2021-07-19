fa177609697420189f58dfbbfa692c94c46ea758
Suporte <suporte@RDM503059.local>
2021-07-19 15:50:19 -0300

    mudanças e ajustes no menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import './index.css'
-import $ from 'jquery'
 
 export default function Menu(){
 
-//testeeeeeee
+    const [checked, setChecked] = React.useState(true);
+
+    function setScroll () {
+        if (checked == true){
+            setChecked(false)
+            document.body.style.overflow = "hidden"
+        }
+        else {
+            setChecked(true)
+            document.body.style.overflow = "auto";
+        }
+    }
+
+
     return(
         <div className="disable">
-            <input type="checkbox" className="tablet-disable" id="check" />
+            <input type="checkbox" className="tablet-disable" id="check"  defaultChecked={false} onChange={() => setScroll()}/>
             <label className= "tablet-disable" for="check" >
-                <div className="menu-button tablet-disable">
+                <div className="menu-button menu-button-2  tablet-disable">
                     <svg className="change-size-svg" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <rect className="change-svg" opacity="0.5" width="24" height="24" rx="12" fill="#28282A"/>
                         <rect className="change-svg" opacity="0.5" x="1.70068" y="13.7953" width="10.2992" height="10.2047" fill="#28282A"/>
@@ -19,10 +31,10 @@ export default function Menu(){
                     </svg>
                 </div>
             </label>
-            <ul className="tablet-disable">
+            <ul className="tablet-disable" id="ul">
                 <p className="name-small">Flávio Pires</p>
                 <p className="name-big">Flávio Pires</p>
-                <li><a className="about-href">Sobre</a></li>
+                <li><a className="about-href" href="#about" onClick={() => setScroll()}>Sobre</a></li>
                 <li><a className="article-href">Artigos</a></li>
                 <li><a className="project-href">Projetos</a></li>
                 <li><a className="contact-href">Contato</a></li>
